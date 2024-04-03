fdafadbe06084a12bb20342cf10fd561bc7b50dd
Kostiantyn <k.varetsa@gmail.com>
2024-04-03 14:26:08 +0200

    Added docstring and type hints

--- simple_meta.py
+++ simple_meta.py
@@ -1,5 +1,16 @@
 class PrintGreet(type):
-    def __new__(cls, clsname, superclasses, attributedict):
+    def __new__(cls, clsname: str, superclasses: tuple, attributedict: dict) -> "PrintGreet":
+        """
+            Metaclass that prints greetings and information when a new class is created.
+
+            Attributes:
+            clsname (str): The name of the class being created.
+            superclasses (tuple): The tuple of superclasses of the class being created.
+            attributedict (dict): The dictionary of attributes of the class being created.
+
+            Returns:
+            Type[type]: The newly created class.
+        """
         print("Greetings!")
         print("Name:", clsname)
         print("Superclasses:", superclasses)
@@ -7,5 +18,14 @@ class PrintGreet(type):
         return type.__new__(cls, clsname, superclasses, attributedict)
 
 
-class New(metaclass=PrintGreet):
-    ...
+def main() -> None:
+    """
+    Main function to demonstrate the usage of a simple metaclass.
+    """
+
+    class New(metaclass=PrintGreet):
+        ...
+
+
+if __name__ == "__main__":
+    main()
